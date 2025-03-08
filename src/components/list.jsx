809655a1d009e91a3ffbf7309d67f0f50183cce0
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-03-08 15:40:53 +0500

    class components

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,19 +1,40 @@
-const List = ({number, title, id}) => {
-    return (
-        <div className='shopping-list'>
-            <div className='list-item'>
-                <div className='item-info'>
-                    <h1>{id}</h1>
-                    <span>{number}</span>
-                    <p>{title}</p>
-                </div>
-                <div className='item-actions'>
-                    <span className='check'>&#10003;</span>
-                    <span className='times'>&times;</span>
+import {Component} from "react";
+
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+        this.increase = this.increase.bind(this)
+    }
+
+    increase() {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+
+
+    render() {
+        const {number, title, id} = this.props;
+        const {counter} = this.state;
+        return (
+            <div className='shopping-list'>
+                <div className='list-item'>
+                    <div className='item-info'>
+                        <h1>{id}</h1>
+                        <span>{number}</span>
+                        <p>{title}</p>
+                    </div>
+                    <div className='item-actions'>
+                        <span className='check'>{counter}</span>
+                        <span className='times'>&times;</span>
+                        <button onClick={this.increase}> Increase</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
 export default List
