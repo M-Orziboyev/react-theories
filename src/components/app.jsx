df7ead30d36b833f86bb4c9788bcde1a67849f6d
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-03-15 13:11:37 +0500

    form inputs

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -34,6 +34,7 @@ class App extends Component {
         })
     }
 
+
     render() {
         const {data} = this.state
         return(
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,11 +1,40 @@
-const ShoppingAddForm = () => {
-	return (
-		<div className='form'>
-			<input type="text" className='title' placeholder='Title...' />
-			<input type='number' className='number' placeholder='14' />
-			<button>Add</button>
-		</div>
-	)
+import React from "react";
+
+class ShoppingAddForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "",
+            number: ""
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onAdd = () => {
+        const data = {
+            title: this.state.title,
+            number: this.state.number
+        }
+        console.log(data)
+    }
+
+    render() {
+        const {title, number} = this.state
+        return (
+            <div className='form'>
+                <input type="text" className='title' placeholder='Title...' name='title' onChange={this.onChange}
+                       value={title}/>
+                <input type='number' className='number' placeholder='14' name='number' onChange={this.onChange}
+                       value={number}/>
+                <button onClick={this.onAdd}>Add</button>
+            </div>
+        )
+    }
 }
 
 export default ShoppingAddForm
