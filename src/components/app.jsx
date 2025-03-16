fc067be89ebfe859e00d066f5008dfe6befe5097
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-03-16 16:07:03 +0500

    Search Panel

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,13 +6,15 @@ import Filter from "./filter.jsx";
 import List from "./list.jsx";
 import {arr} from "./constants/index.js";
 import {v4 as uuidv4} from 'uuid';
+import SearchPanel from "./searchPanel.jsx";
 
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: arr
+            data: arr,
+            search: ""
         }
     }
 
@@ -44,16 +46,31 @@ class App extends Component {
         })
     }
 
+    searchData = (arr, term) => {
+        if (term.length === 0){
+            return arr
+        }
+
+        return arr.filter(item => item.title.toLowerCase().indexOf(term.toLowerCase()) > -1)
+    }
+
+    onUpdateSearch = (search) => {
+        this.setState({search})
+    }
+
 
     render() {
-        const {data} = this.state
+        const {data, search} = this.state
+        const allData = this.searchData(data, search)
+
         return (
             <div className="app">
                 <div className="wrapper">
                     <div className="card">
                         <Information length={data.length}/>
+                        <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                         <Form onAdd={this.onAdd}/>
-                        <List data={data} onDelete={this.onDelete} onToggleItem={this.onToggleActive}/>
+                        <List data={allData} onDelete={this.onDelete} onToggleItem={this.onToggleActive}/>
                         <Filter/>
                     </div>
                     <img src="/images/earth.svg" alt="earth"/>
